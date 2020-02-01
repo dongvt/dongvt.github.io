6867edfe7ce029d25db1f530741dc1bdb67a0f05
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2020-01-31 17:52:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ const quiz = [
 const view = new View();
 const game = new Game();
 
+this.score = document.querySelector('#score strong')
+this.question = document.getElementById('question')
+this.result = document.getElementById('result')
+this.info = document.getElementById('info')
+this.start = document.getElementById('start')
+this.response = document.getElementById('response')
+
 view.start.addEventListener('click', () => game.start(quiz), false);
 view.response.addEventListener('submit', (event) => game.check(event), false);
 view.hide(view.response);
