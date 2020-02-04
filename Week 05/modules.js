35d45c0147dc64735f533a8eaea9c0cbccbbd08d
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2020-02-04 13:54:29 -0700

    Week 05
    First commit team activity

---
+++ Week 05/modules.js
@@ -0,0 +1,35 @@
+const imgBasePath = 'http://byui-cit.github.io/cit261/examples/';
+
+export class Hikes {
+
+    
+    showHikeList(hikeListElement) {
+        hikeListElement.innerHTML = '';
+        renderHikeList(hikeList, hikeListElement);
+    }
+
+    renderHikeList(hikes, parent) {
+        hikes.forEach(hike => {
+            parent.appendChild(renderOneHike(hike));
+        });
+    }
+
+    renderOneHike(hike) {
+        const item = document.createElement('li');
+
+        item.innerHTML = ` <h2>${hike.name}</h2>
+          <div class="image"><img src="${imgBasePath}${hike.imgSrc}" alt="${hike.imgAlt}"></div>
+          <div>
+                  <div>
+                      <h3>Distance</h3>
+                      <p>${hike.distance}</p>
+                  </div>
+                  <div>
+                      <h3>Difficulty</h3>
+                      <p>${hike.difficulty}</p>
+                  </div>
+          </div>`;
+
+        return item;
+    }
+}
